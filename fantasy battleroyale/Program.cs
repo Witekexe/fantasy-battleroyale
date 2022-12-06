a0ee3502a3cf083e2c4505bc2a0927f5174a3941
witekexe <witek.exe@gmail.com>
2022-12-06 22:59:07 +0100

    Add project files.

---
+++ fantasy battleroyale/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace FantasyGame
+{
+
+    internal class game
+    { 
+       
+
+        public class enemy
+        {
+            int MAX_HP = 100;
+            int HP = MAX_HP - damagetaken;
+            int damage = 10;
+            int mana = 0;
+            int damagetaken = damagetaken; //+ damage enemy//;
+            // isparalised = False//
+
+
+            public class fighter
+        {
+                int MAX_HP = 100;
+                int HP = MAX_HP - damagetaken;
+                int damage = 10;
+                int mana = 0;
+                int damagetaken = damagetaken; //+ damage enemy//;
+                // isparalised = False//
+            }
+            public class knight
+        {
+                int MAX_HP = 150;
+                int HP = MAX_HP - damagetaken;
+                int damage = 8;
+                int mana = 0;
+                int damagetaken = damagetaken; //+ damage enemy//;
+                // isparalised = False//
+            }
+            public class sorcerer
+        {
+                int MAX_HP = 100;
+                int HP = MAX_HP - damagetaken;
+                int damage = mana ;
+                int mana = 100 - //25 kazdy cyklus//;
+                int damagetaken = damagetaken; //+ damage enemy//;
+                // isparalised = False//
+            }
+        public class beast
+        {
+                int MAX_HP = 100;
+                int HP = MAX_HP - damagetaken;
+                int damage = 10;
+                int mana = 0;
+                int damagetaken = damagetaken; //+ damage enemy//;
+                //set isparalised//
+            }
+    
+
+    }
+}
